c622e1bec6a317424fa8aa92854c5e9f01caa31f
BuyBuy <buybuy@example.com>
2025-12-29 17:05:41 +0800

    Auto-run migrations on startup

--- buybuy/wsgi.py
+++ buybuy/wsgi.py
@@ -3,10 +3,16 @@ WSGI config for buybuy project.
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
+import sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buybuy.settings')
 
-application = get_wsgi_application()
+# Run migrations on startup (for Railway deployment)
+if os.environ.get('RAILWAY_ENVIRONMENT'):
+    print("Railway detected, running migrations...")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'migrate', '--noinput'])
+    print("Migrations complete!")
 
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
